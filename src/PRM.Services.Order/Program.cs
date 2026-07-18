6d568dbea5393995cf21574b42a6188dddad3c21
Nguyen Dang Khoa <khoandse184762@fpt.edu.vn>
2026-07-18 20:08:53 +0700

    Increase SignalR HandshakeTimeout to 60s on Order service

--- src/PRM.Services.Order/Program.cs
+++ src/PRM.Services.Order/Program.cs
@@ -14,7 +14,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSignalR();
+builder.Services.AddSignalR(options =>
+{
+    options.HandshakeTimeout = TimeSpan.FromSeconds(60);
+});
 
 // Database
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
